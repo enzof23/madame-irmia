4eaf892de7a756f267d6f3fd99de610baf209aaf
enzo filippo <enzo.filippo23@gmail.com>
2023-06-26 19:03:57 +1200

    new request log

--- app/(auth)/auth/create_stripe_customer/route.ts
+++ app/(auth)/auth/create_stripe_customer/route.ts
@@ -1,13 +1,14 @@
 import Stripe from "stripe";
 import { supabaseRouteHandler } from "@/supabase-clients/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2022-11-15",
 });
 
-export async function POST(req: any) {
-  console.log({ req });
+export async function POST(req: NextRequest) {
+  const body = req.body;
+  console.log(body);
   // const customer = await stripe.customers.create({
   //   name: req.body.record.auth_id,
   //   email: req.body.record.email,
