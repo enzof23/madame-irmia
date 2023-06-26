d8c9ede7c41ff5863f0ba40e95c37e182cd5a10d
enzo filippo <enzo.filippo23@gmail.com>
2023-06-26 19:47:03 +1200

    update logic in create_stripe_customer to get user data from supabase and not request

---
+++ app/(auth)/auth/create_stripe_customer/route.ts
@@ -0,0 +1,42 @@
+import { supabaseRouteHandler } from "@/supabase-clients/server";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function POST() {
+  try {
+    const supabase = supabaseRouteHandler();
+    const { data, error: profile_error } = await supabase
+      .from("profiles")
+      .select("*");
+
+    if (!data || profile_error || data.length)
+      throw new Error(`An error occured when getting user profiles row`);
+
+    const { auth_id, email } = data[0];
+
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+      apiVersion: "2022-11-15",
+    });
+
+    const customer = await stripe.customers.create({
+      name: auth_id,
+      email,
+    });
+
+    const { error: credits_error } = await supabase
+      .from("user_credits")
+      .update({ stripe_customer: customer.id })
+      .eq("user_id", auth_id);
+
+    if (credits_error)
+      throw new Error(`An error occured when updating user_credits row`);
+
+    return NextResponse.json({
+      message: "Stripe customer was successfully created!",
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error,
+    });
+  }
+}
